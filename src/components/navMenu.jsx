d886e7dda3ce549b87155a4d69472b2e922b1ec5
Grant Watson <grant@triumphdesigns.org>
2018-08-17 14:32:44 -0400

    Deletion of Notes app from this utility. Working on another frame

--- src/components/navMenu.jsx
+++ src/components/navMenu.jsx
@@ -3,8 +3,6 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Home } from "../pages/home";
 import { ToDo } from "../pages/todo";
-import { Notes } from "../pages/notes";
-import { PreviousNotes } from "../pages/previousNotes";
 
 const NaviRoute = () => (
   <Router>
@@ -22,24 +20,12 @@ const NaviRoute = () => (
               ToDo List
             </Link>
           </li>
-          <li>
-            <Link to={"/notes"} className="nav-link">
-              Notes
-            </Link>
-          </li>
-          <li>
-            <Link to={"/previous"} className="nav-link">
-              Previous Notes
-            </Link>
-          </li>
         </ul>
       </nav>
       <hr />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/todo" component={ToDo} />
-        <Route path="/notes" component={Notes} />
-        <Route path="/previous" component={PreviousNotes} />
       </Switch>
     </div>
   </Router>
