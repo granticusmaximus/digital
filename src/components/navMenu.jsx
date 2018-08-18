5a053afdeb6316f50c8f33638743baae05d5f07c
Grant Watson <grant@triumphdesigns.org>
2018-08-18 18:41:24 -0400

    Minor cosmetic changes

--- src/components/navMenu.jsx
+++ src/components/navMenu.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Home } from "../pages/home";
 import { ToDo } from "../pages/todo";
+import { Note } from "../pages/Note";
 
 const NaviRoute = () => (
   <Router>
@@ -20,12 +21,18 @@ const NaviRoute = () => (
               ToDo List
             </Link>
           </li>
+          <li>
+            <Link to={"/note"} className="nav-link">
+              Notes
+            </Link>
+          </li>
         </ul>
       </nav>
       <hr />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/todo" component={ToDo} />
+        <Route path="/note" component={Note} />
       </Switch>
     </div>
   </Router>
--- src/pages/Note.jsx
+++ src/pages/Note.jsx
@@ -1,13 +1,18 @@
+//not_id
+//title
+//noteattribute
+//notemessage
+//created_on
+//update_on
 import "../App.css";
 import React from "react";
-import TodoList from "../components/ToDoList";
 import { Jumbotron } from "reactstrap";
 
-export const ToDo = () => (
+export const Note = () => (
   <div className="container">
     <Jumbotron>
-      <h1 className="display-3">ToDo Page</h1>
-      <p className="lead">This is Grant's ToDo utility</p>
+      <h1 className="display-3">Notes Page</h1>
+      <p className="lead">This is Grant's note utility</p>
       <hr className="my-2" />
       <p>I will add more to this utility as time passes by</p>
       <p>
@@ -15,12 +20,11 @@ export const ToDo = () => (
         on here are open source, but the data is owned by Grant Watson
       </p>
       <hr className="my-2" />
-      <p>This is a simple centralized place for all of my todo items</p>
+      <p>This is a simple centralized place for all of my notes</p>
       <p>
         I will add features to this over time, for example to see previously
-        deleted todos, adding attributes to todos, etc.
+        deleted notes, adding attributes to notes, etc.
       </p>
     </Jumbotron>
-    <TodoList />
   </div>
 );
